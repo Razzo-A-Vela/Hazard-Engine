be44ac2a19b2c2bf44e06d7120b4c2574b0890b8
Razzo_A_Vela <giumifed@gmail.com>
2023-11-09 14:57:31 +0100

    Added copy and +operator

--- src/types/Vector2.hpp
+++ src/types/Vector2.hpp
@@ -1,15 +1,43 @@
 #pragma once
 
 struct Vector2 {
-  Vector2(float x = 0, float y = 0) : x(x), y(y) {}
-
   float x;
   float y;
+  Vector2(float x = 0, float y = 0) : x(x), y(y) {}
+
+  Vector2 operator+(Vector2 other) {
+    Vector2 ret = copy();
+    ret.x += other.x;
+    ret.y += other.y;
+    return ret;
+  }
+
+  Vector2 operator+(float f) {
+    return (*this) + Vector2(f, f);
+  }
+
+  Vector2 copy() {
+    return Vector2(x, y);
+  }
 };
 
 struct Vector2Int {
-  Vector2Int(int x = 0, int y = 0) : x(x), y(y) {}
-
   int x;
   int y;
+  Vector2Int(int x = 0, int y = 0) : x(x), y(y) {}
+
+  Vector2Int operator+(Vector2Int other) {
+    Vector2Int ret = copy();
+    ret.x += other.x;
+    ret.y += other.y;
+    return ret;
+  }
+
+  Vector2Int operator+(int i) {
+    return (*this) + Vector2Int(i, i);
+  }
+
+  Vector2Int copy() {
+    return Vector2Int(x, y);
+  }
 };
