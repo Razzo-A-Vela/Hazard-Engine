afaff07e657d05697fb31f57f48a2cf0cea1decb
Razzo_A_Vela <giumifed@gmail.com>
2023-11-15 17:25:36 +0100

    Added default values for Rect constructor

--- src/types/Rect.hpp
+++ src/types/Rect.hpp
@@ -9,7 +9,7 @@ struct RectInt;
 struct Rect {
   Vector2 pos;
   Vector2 size;
-  Rect(Vector2 pos, Vector2 size) : pos(pos), size(size) {}
+  Rect(Vector2 pos = { 0, 0 }, Vector2 size = { 0, 0 }) : pos(pos), size(size) {}
 
   bool collidesWith(Vector2 other) const;
   bool collidesWith(Rect other) const;
@@ -21,7 +21,7 @@ struct Rect {
 struct RectInt {
   Vector2Int pos;
   Vector2Int size;
-  RectInt(Vector2Int pos, Vector2Int size) : pos(pos), size(size) {}
+  RectInt(Vector2Int pos = { 0, 0 }, Vector2Int size = { 0, 0 }) : pos(pos), size(size) {}
 
   bool collidesWith(Vector2Int other) const;
   bool collidesWith(RectInt other) const;
